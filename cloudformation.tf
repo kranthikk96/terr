461e0f1ab4e033cbde106e99743d8f522da02b07
kranthikk96 <87638903+kranthikk96@users.noreply.github.com>
2021-11-25 11:25:45 +0530

    Update cloudformation.tf

--- cloudformation.tf
+++ cloudformation.tf
@@ -1,66 +1,19 @@
 resource "aws_cloudformation_stack" "network" {
   name = "networking-stack"
 
-  parameters:
-    InstanceCount:
-      Description: Number of EC2 instances (must be between 1 and 5)
-      Type: Number
-      Default: 1
-      MinValue: 1
-      MaxValue: 5
-      ConstraintDescription: Must be a number between 1 and 5.
+ parameters = {
+    VPCCidr = "10.0.0.0/16"
     ImageId:
-      Description: Image ID to launch EC2 instances.
-      Type: AWS::EC2::Image::Id
-      # amzn-ami-hvm-2016.09.1.20161221-x86_64-gp2
-      Default: ami-9be6f38c
-    InstanceType:
-      Description: Instance type to launch EC2 instances.
-      Type: String
-      Default: t2.micro
-      AllowedValues: [ t2.micro,m3.medium, m3.large, m3.xlarge, m3.2xlarge ]
-  Conditions:
-    Launch1: !Equals [1, 1]
-    Launch2: !Not [!Equals [1, !Ref InstanceCount]]
-    Launch3: !And
-    - !Not [!Equals [1, !Ref InstanceCount]]
-    - !Not [!Equals [2, !Ref InstanceCount]]
-    Launch4: !Or
-    - !Equals [4, !Ref InstanceCount]
-    - !Equals [5, !Ref InstanceCount]
-    Launch5: !Equals [5, !Ref InstanceCount]
-  Resources:
-    Instance1:
-      Condition: Launch1
-      Type: AWS::EC2::Instance
-      Properties:
-        ImageId: !Ref ImageId
-        InstanceType: !Ref InstanceType
-   Instance2:
-     Condition: Launch2
-     Type: AWS::EC2::Instance
-     Properties:
-       ImageId: !Ref ImageId
-       InstanceType: !Ref InstanceType
-   Instance3:
-     Condition: Launch3
-     Type: AWS::EC2::Instance
-     Properties:
-       ImageId: !Ref ImageId
-       InstanceType: !Ref InstanceType
-   Instance4:
-     Condition: Launch4
-     Type: AWS::EC2::Instance
-     Properties:
-       ImageId: !Ref ImageId
-       InstanceType: !Ref InstanceType
-   Instance5:
-     Condition: Launch5
-     Type: AWS::EC2::Instance
-     Properties:
-       ImageId: !Ref ImageId
-       InstanceType: !Ref InstanceType
-  
+    Description: Image ID to launch EC2 instances.
+    Type: AWS::EC2::Image::Id
+    # amzn-ami-hvm-2016.09.1.20161221-x86_64-gp2
+    Default: ami-9be6f38c
+  InstanceType:
+    Description: Instance type to launch EC2 instances.
+    Type: String
+    Default: m3.medium
+    AllowedValues: [ m3.medium, m3.large, m3.xlarge, m3.2xlarge ]
+  }
 
   template_body = <<STACK
 {
@@ -79,6 +32,30 @@ resource "aws_cloudformation_stack" "network" {
         "Tags" : [
           {"Key": "Name", "Value": "Primary_CF_VPC"}
         ]
+   "Resources" : {
+		"Web1" : 
+		{
+			"Type":"AWS::EC2::Instance",
+			"Properties":
+			{
+				"ImageId":"ami-08111162", 
+				"InstanceType":"t2.micro", 
+				"SecurityGroups":
+				[
+					{
+						"Ref":"WebSecuirtyGroup"
+					}
+				], 
+				"KeyName":"devopstest",
+				"Tags": 
+				[
+					{
+						"Key":"Dev",
+						"Value":"Dev"
+					}
+				]
+			}
+		}     
       }
     }
   }
