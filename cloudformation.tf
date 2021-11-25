50af98c491bc2ca1ada6605b4bacddce8d2d36ba
kranthikk96 <87638903+kranthikk96@users.noreply.github.com>
2021-11-25 20:23:58 +0530

    Update cloudformation.tf

--- cloudformation.tf
+++ cloudformation.tf
@@ -4,6 +4,25 @@ resource "aws_cloudformation_stack" "network" {
  parameters = {
     VPCCidr = "10.0.0.0/16"
   }
+Parameters:
+  KeyName:
+    Description: Name of an existing EC2 KeyPair to enable SSH access to the instance
+    Type: AWS::EC2::KeyPair::KeyName
+    ConstraintDescription: must be the name of an existing EC2 KeyPair.
+  InstanceType:
+    Description: WebServer EC2 instance type
+    Type: String
+    Default: t2.micro
+    AllowedValues: [t2.nano, t2.micro, t2.small, t2.medium, t2.large, t2.xlarge, t2.2xlarge,
+      t3.nano, t3.micro, t3.small, t3.medium, t3.large, t3.xlarge, t3.2xlarge,
+      m4.large, m4.xlarge, m4.2xlarge, m4.4xlarge, m4.10xlarge,
+      m5.large, m5.xlarge, m5.2xlarge, m5.4xlarge,
+      c5.large, c5.xlarge, c5.2xlarge, c5.4xlarge, c5.9xlarge,
+      g3.8xlarge,
+      r5.large, r5.xlarge, r5.2xlarge, r5.4xlarge, r3.12xlarge,
+      i3.xlarge, i3.2xlarge, i3.4xlarge, i3.8xlarge,
+      d2.xlarge, d2.2xlarge, d2.4xlarge, d2.8xlarge]
+    ConstraintDescription: must be a valid EC2 instance type.
 
   template_body = <<STACK
 {
@@ -22,21 +41,37 @@ resource "aws_cloudformation_stack" "network" {
         "Tags" : [
           { "Key" : "Name", "Value" : "Primary_CF_VPC" }
         ]
-   "Resources" : {
-     "Web1" : {
-	"Type": "AWS::EC2::Instance",
-	"Properties" : {
-	  "ImageId" : "ami-083654bd07b5da81d", 
-	  "InstanceType" : "t2.micro", 
-	  "SecurityGroups" : [
-	     { "Ref" : "web_sg" }
-	  ],
-	  "Tags" : [
-	    { "Key" : "Name", "Value" : "Primary_CF_EC2" }
-	  ]
-	  }
-	}
-       }
+  Resources:
+  EC2Instance:
+    Type: AWS::EC2::Instance
+    Properties:
+      InstanceType: !Ref 'InstanceType'
+      SecurityGroups: [!Ref 'InstanceSecurityGroup']
+      KeyName: !Ref 'KeyName'
+      ImageId: !Ref 'LatestAmiId'
+  InstanceSecurityGroup:
+    Type: AWS::EC2::SecurityGroup
+    Properties:
+      GroupDescription: Enable SSH access via port 22
+      SecurityGroupIngress:
+      - IpProtocol: tcp
+        FromPort: 22
+        ToPort: 22
+        CidrIp: !Ref 'SSHLocation'
+Outputs:
+  InstanceId:
+    Description: InstanceId of the newly created EC2 instance
+    Value: !Ref 'EC2Instance'
+  AZ:
+    Description: Availability Zone of the newly created EC2 instance
+    Value: !GetAtt [EC2Instance, AvailabilityZone]
+  PublicDNS:
+    Description: Public DNSName of the newly created EC2 instance
+    Value: !GetAtt [EC2Instance, PublicDnsName]
+  PublicIP:
+    Description: Public IP address of the newly created EC2 instance
+    Value: !GetAtt [EC2Instance, PublicIp] 
+       
       }
     }
   }
