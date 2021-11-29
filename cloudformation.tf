bfb030e39d04fefb53ad522ae3efa4ae17d65975
kranthikk96 <87638903+kranthikk96@users.noreply.github.com>
2021-11-29 10:24:47 +0530

    Update cloudformation.tf

--- cloudformation.tf
+++ cloudformation.tf
@@ -1,31 +1,29 @@
-resource "aws_cloudformation_stack" "network" {
-  name = "networking-stack"
+data "aws_iam_policy_document" "AWSCloudFormationStackSetAdministrationRole_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
 
- parameters = {
+    principals {
+      identifiers = ["cloudformation.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role" "AWSCloudFormationStackSetAdministrationRole" {
+  assume_role_policy = data.aws_iam_policy_document.AWSCloudFormationStackSetAdministrationRole_assume_role_policy.json
+  name               = "AWSCloudFormationStackSetAdministrationRole"
+}
+
+resource "aws_cloudformation_stack_set" "example" {
+  administration_role_arn = aws_iam_role.AWSCloudFormationStackSetAdministrationRole.arn
+  name                    = "example"
+
+  parameters = {
     VPCCidr = "10.0.0.0/16"
   }
- Parameters = {
-  KeyName :
-    Description : Name of an existing EC2 KeyPair to enable SSH access to the instance
-    Type: AWS::EC2::KeyPair::KeyName
-    ConstraintDescription: must be the name of an existing EC2 KeyPair.
-  InstanceType:
-    Description: WebServer EC2 instance type
-    Type: String
-    Default: t2.micro
-    AllowedValues : [t2.nano, t2.micro, t2.small, t2.medium, t2.large, t2.xlarge, t2.2xlarge,
-      t3.nano, t3.micro, t3.small, t3.medium, t3.large, t3.xlarge, t3.2xlarge,
-      m4.large, m4.xlarge, m4.2xlarge, m4.4xlarge, m4.10xlarge,
-      m5.large, m5.xlarge, m5.2xlarge, m5.4xlarge,
-      c5.large, c5.xlarge, c5.2xlarge, c5.4xlarge, c5.9xlarge,
-      g3.8xlarge,
-      r5.large, r5.xlarge, r5.2xlarge, r5.4xlarge, r3.12xlarge,
-      i3.xlarge, i3.2xlarge, i3.4xlarge, i3.8xlarge,
-      d2.xlarge, d2.2xlarge, d2.4xlarge, d2.8xlarge]
-    ConstraintDescription: must be a valid EC2 instance type.
-   }
 
-  template_body = <<STACK
+  template_body = <<TEMPLATE
 {
   "Parameters" : {
     "VPCCidr" : {
@@ -40,42 +38,25 @@ resource "aws_cloudformation_stack" "network" {
       "Properties" : {
         "CidrBlock" : { "Ref" : "VPCCidr" },
         "Tags" : [
-          { "Key" : "Name", "Value" : "Primary_CF_VPC" }
+          {"Key": "Name", "Value": "Primary_CF_VPC"}
         ]
+      }
     }
   }
 }
-STACK
+TEMPLATE
 }
- Resources:
-  EC2Instance:
-    Type: AWS::EC2::Instance
-    Properties:
-      InstanceType: !Ref 'InstanceType'
-      SecurityGroups: [!Ref 'InstanceSecurityGroup']
-      KeyName: !Ref 'KeyName'
-      ImageId: !Ref 'LatestAmiId'
-  InstanceSecurityGroup:
-    Type: AWS::EC2::SecurityGroup
-    Properties:
-      GroupDescription: Enable SSH access via port 22
-      SecurityGroupIngress:
-      - IpProtocol: tcp
-        FromPort: 22
-        ToPort: 22
-        CidrIp: !Ref 'SSHLocation'
-Outputs:
-  InstanceId:
-    Description: InstanceId of the newly created EC2 instance
-    Value: !Ref 'EC2Instance'
-  AZ:
-    Description: Availability Zone of the newly created EC2 instance
-    Value: !GetAtt [EC2Instance, AvailabilityZone]
-  PublicDNS:
-    Description: Public DNSName of the newly created EC2 instance
-    Value: !GetAtt [EC2Instance, PublicDnsName]
-  PublicIP:
-    Description: Public IP address of the newly created EC2 instance
-    Value: !GetAtt [EC2Instance, PublicIp] 
-       
-      }
+
+data "aws_iam_policy_document" "AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy" {
+  statement {
+    actions   = ["sts:AssumeRole"]
+    effect    = "Allow"
+    resources = ["arn:aws:iam::*:role/${aws_cloudformation_stack_set.example.execution_role_name}"]
+  }
+}
+
+resource "aws_iam_role_policy" "AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy" {
+  name   = "ExecutionPolicy"
+  policy = data.aws_iam_policy_document.AWSCloudFormationStackSetAdministrationRole_ExecutionPolicy.json
+  role   = aws_iam_role.AWSCloudFormationStackSetAdministrationRole.name
+}
